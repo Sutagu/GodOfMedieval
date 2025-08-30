e8a566642fb9d2f6efcad988891bb1337c3397ff
Pete <134797466+Sutagu@users.noreply.github.com>
2025-08-30 19:11:24 +0800

    Created a simple "ai" script for the character for randomised rng movement. May need to tweak the speed or range of travel

--- Assets/Scripts/warrior_blue_movement.cs
+++ Assets/Scripts/warrior_blue_movement.cs
@@ -1,36 +1,70 @@
 using UnityEngine;
+using System.Collections;
 
 public class warrior_blue_movement : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 2;
     public int facingDirection = 1;
     public Rigidbody2D rb;
     public Animator anim;
-
+    public float updateInterval = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(updateRandomMovement()); 
+    }
+
+    private IEnumerator updateRandomMovement()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(updateInterval);
+
+            //float horizontal = (Random.Range(0, 1f) > 0.5 ? Random.Range(5f,10f) : Random.Range(-10f,-5f));
+            //float vertical = (Random.Range(0, 1f) > 0.5 ? Random.Range(5f, 10f) : Random.Range(-10f, -5f));
+      
+                float horizontal = Random.Range(-0.5f,0.5f);
+                float vertical = Random.Range(-0.5f,0.5f);
+
+                if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+                {
+                    Flip();
+                }
+
+                anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+                anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+
+                rb.linearVelocity = new Vector2(horizontal, vertical).normalized * speed;
+
+                yield return new WaitForSeconds(3f);
+                rb.linearVelocity = Vector2.zero;
+            horizontal = 0;
+            vertical = 0;
+            anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+            anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+    //void FixedUpdate()
+    //{
+    //    float horizontal = Input.GetAxis("Horizontal");
+    //    float vertical = Input.GetAxis("Vertical");
 
-        if(horizontal > 0 && transform.localScale.x < 0 ||  horizontal < 0 && transform.localScale.x > 0)
-        {
-            Flip();
-        }
+    //    if(horizontal > 0 && transform.localScale.x < 0 ||  horizontal < 0 && transform.localScale.x > 0)
+    //    {
+    //        Flip();
+    //    }
 
-        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
-        anim.SetFloat("vertical", Mathf.Abs(vertical));
+    //    anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+    //    anim.SetFloat("vertical", Mathf.Abs(vertical));
 
 
-        rb.linearVelocity = new Vector2(horizontal, vertical).normalized * speed;
-    }
+    //    rb.linearVelocity = new Vector2(horizontal, vertical).normalized * speed;
+    //}
 
     void Flip() 
     {

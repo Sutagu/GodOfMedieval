472435b1655150e2e3c86f5cedb74e614993ed26
Pete <134797466+Sutagu@users.noreply.github.com>
2025-09-02 14:54:38 +0800

    Allowed simpler access of entry and exit rather than 2 box colliders per stairs, as the distance between entry and exit points were too close.

--- Assets/Scripts/tilemap_scripts/elevation_entry.cs
+++ Assets/Scripts/tilemap_scripts/elevation_entry.cs
@@ -2,33 +2,33 @@ using UnityEngine;
 
 public class elevation_entry : MonoBehaviour
 {
-    //public Collider2D[] mountainColliders;
-    //public Collider2D[] mountainBoundary;
+
     public string CharacterElevated = "Character Elevated";
+    public string Character = "Character";
     public void Start()
     {
         Debug.Log("Entry script is working");
     }
-    public void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Character has touched the entry");
         if (collision.gameObject.layer == LayerMask.NameToLayer("Character"))
         {
             Debug.Log("Character layer is indeed equal to Character, now converting to Elevated");
             collision.gameObject.layer = LayerMask.NameToLayer(CharacterElevated);
 
             Debug.Log(LayerMask.LayerToName(collision.gameObject.layer));
-            //foreach (Collider2D mountain in mountainColliders)
-            //{
-            //    Physics2D.IgnoreCollision(mountain, other, true);
 
-            //}
-            //foreach (Collider2D boundary in mountainBoundary)
-            //{
-            //    Physics2D.IgnoreCollision(boundary, other, false);
-
-            //}
             collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+            return;
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Character Elevated"))
+        {
+            Debug.Log("Character layer is indeed equal to Character Elevated, now converting to Character");
+            collision.gameObject.layer = LayerMask.NameToLayer(Character);
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+
+            Debug.Log(LayerMask.LayerToName(collision.gameObject.layer));
+            return;
         }
     }
 }
